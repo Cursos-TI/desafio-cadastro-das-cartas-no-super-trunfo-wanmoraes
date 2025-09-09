b6f55fd3046bb38e4eff43dd7bca11a1c0bdeb7b
wanmoraes <Nemofantasia_rrn@hotmail.com>
2025-09-09 14:53:47 +0000

    escrevendo printf e sacanf

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(){
+    char Estado = 'A';
+    char Estado = 'E';
+    char Codigodacarta = 'A01'[20];
+    char Codigodacarta = 'E02'[20];
+    char Nomedacidade1 = 'Salvador'[50];
+    char Nomedacidade2 = 'Amapa'[50];
+    int Populacao1 = 200000;
+    int Populacao2 = 350000;
+    float Area1 = 152.00;km²;
+    float Area2 = 253.00;km;
+    float Pib1 = 456.00;
+    float Pib2 = 359.00;
+    float Pontos turisticos = 50;
+    float Pontos turisticos = 30;
+
+
+    return 0;
+    
+}
